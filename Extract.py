7404b85e061c621df06e72f50fe97370092e8d1c
JaktensTid <jaktenstid1@gmail.com>
2016-12-02 20:11:23 +0300

    Balance

--- Extract.py
+++ Extract.py
@@ -1,13 +1,17 @@
 import asyncio
-import datetime
+from multiprocessing import cpu_count
 from multiprocessing.pool import Pool
 from aiohttp import ClientSession
 import csv
 import json
+import py_compile
+py_compile.compile('Extract.py')
 
 MAX_USERS = 1200000000
 USER_URL = 'http://facebook.com/profile.php?id=%s'
 GRAPH_URL = 'http://graph.facebook.com/%s/picture?width=100&height=100&redirect=false'
+MAX_CHUNK_LENGTH = 100000
+MIN_MULTIPROCESSED_LENGTH = 400000
 
 async def fetch(url, id, session):
     async with session.get(url) as response:
@@ -29,7 +33,7 @@ async def run(begin, end, file_end):
 
         responses = asyncio.gather(*tasks)
         await responses
-        with open('result%s.csv' % file_end, 'a') as csvfile:
+        with open('Facebook faces %s.csv' % file_end, 'a') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',',
                                     quotechar='"', quoting=csv.QUOTE_MINIMAL)
             spamwriter.writerow(['UserID','AvatarURL'])
@@ -42,18 +46,34 @@ async def run(begin, end, file_end):
 def threaded(t):
     loop = asyncio.get_event_loop()
     future = asyncio.ensure_future(run(t[0],t[1],t[2]))
-    result = loop.run_until_complete(future)
+    loop.run_until_complete(future)
 
 
-def main():
-    begin_date = datetime.datetime.now()
-    print('BEGIN ' + str(begin_date))
-    p = Pool(4)
-    # begin end file_end
-    print(p.map(threaded, [(0,1000,0),(1001,2000,1),(2001,3000,2),(3001,4000,3)]))
-    print('TOTAL ' + str(datetime.datetime.now() - begin_date))
-    print('END')
+def main(begin, end):
+    if begin > end:
+        t = end
+        end = begin
+        begin = t
+    count = end - begin
+    parts = cpu_count()
+    if count > MIN_MULTIPROCESSED_LENGTH:
+        balance = 0
+        chunk_size = int((end - begin)/parts)
+        if chunk_size > MAX_CHUNK_LENGTH:
+            chunk_size = MAX_CHUNK_LENGTH
+            parts = int(count/chunk_size)
+        if (end - begin) % parts != 0:
+            balance = end - (chunk_size * parts)
+        creator = lambda b,e: (b,e,str(b) + '-' + str(e))
+        chunks = [creator(int((begin + chunk_size * i) + 1),int(begin + chunk_size * (i+1)))
+                  for i in range(parts)] + [creator(end - balance + 1 + begin, end)] if balance != 0 else []
+        p = Pool(cpu_count())
+        # begin end file_end
+        p.map(threaded, chunks)
+    else:
+        threaded((begin,end,str(begin) + '-' + str(end)))
+    print('ENDED PROCESSING OF ' + str(end - begin) + ' URLS')
 
 
 if __name__ == '__main__':
-    main()
+    main(11,18353243)
